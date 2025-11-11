bace0e32c66e6f436c9c407c491461a59a4a1ab3
aguer47 <maguer@byupathway.edu>
2025-11-11 23:49:14 +0300

    commit

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        // Creating fractions using all three constructors
+        Fraction f1 = new Fraction();       
+        Fraction f2 = new Fraction(5);      
+        Fraction f3 = new Fraction(3, 4);   
+        Fraction f4 = new Fraction(1, 3);   
+
+        // Display results
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        // Testing setters
+        f1.SetTop(7);
+        f1.SetBottom(8);
+        Console.WriteLine(f1.GetFractionString()); 
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,58 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    // Constructor 1: no parameter
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor 2: one parameter 
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    // Constructor 3: two parameters
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
