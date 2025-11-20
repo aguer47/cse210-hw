935acecad029fc10701672bf2d44b81fa377e8e6
aguer47 <maguer@byupathway.edu>
2025-11-20 13:01:55 +0300

    commit

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+
+        // ORDER 1.
+        Address address1 = new Address("123 Main St", "Phoenix", "AZ", "USA");
+        Customer customer1 = new Customer("John Chol", address1);
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Laptop", "A101", 700, 1));
+        order1.AddProduct(new Product("Mouse", "B204", 25, 2));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.GetTotalPrice()}\n");
+
+        // ORDER 2.
+        Address address2 = new Address("45 Regent Rd", "London", "London", "UK");
+        Customer customer2 = new Customer("Emmy Clark", address2);
+        Order order2 = new Order(customer2);
+
+        order2.AddProduct(new Product("Headphones", "C310", 50, 3));
+        order2.AddProduct(new Product("Microphone", "D450", 120, 1));
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.GetTotalPrice()}");
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+        List<Video> videos = new List<Video>();
+
+        // Video 1.
+        Video v1 = new Video("Exploring Uganda", "TravelWithMajok", 420);
+        v1.AddComment(new Comment("Aduto", "Great video bambi"));
+        v1.AddComment(new Comment("John", "Uganda looks beautiful OMG"));
+        v1.AddComment(new Comment("Derrick", "Can not wait to visit the pearl of Africa!"));
+        videos.Add(v1);
+
+        // Video 2.
+        Video v2 = new Video("python Tutorial Basics", "CodeMaster", 600);
+        v2.AddComment(new Comment("Bino", "Very helpful lesson."));
+        v2.AddComment(new Comment("Sarah", "Thank you for explaining so clearly."));
+        v2.AddComment(new Comment("Mike", "Please do  part two sir."));
+        videos.Add(v2);
+
+        // Video 3.
+        Video v3 = new Video("Healthy Living Tips", "WellnessWorld", 350);
+        v3.AddComment(new Comment("Grace", "Love these tips!"));
+        v3.AddComment(new Comment("Kim", "So motivating."));
+        v3.AddComment(new Comment("Peter", "I learned a lot."));
+        videos.Add(v3);
+
+        // Display all videos
+        foreach (Video v in videos)
+        {
+            v.DisplayVideoInfo();
+        }
     }
 }
