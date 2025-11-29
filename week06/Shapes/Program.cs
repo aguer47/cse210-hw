5c538cd02d8cf3e449c620704faa9df797226573
aguer47 <maguer@byupathway.edu>
2025-11-29 11:36:19 +0300

    commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}  |  Area: {s.GetArea()}");
+        }
     }
 }
